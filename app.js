58b74c725cd0f58b8637e05866443001518ebe21
raj2208 <rajkrai22@gmail.com>
2023-08-08 23:49:01 +0530

    first-commit

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-//You will build this project using pure Node to gain a thorough understanding of Node. There are obviously easier ways of building this project using external modules, but that is the purpose of this project as you would miss out on many code concepts.
-
-
 //require node modules
-const http = require('http');
+const http = require("http");
 
 //file imports
-const respond = require('./lib/respond.js');
+const respond = require("./lib/respond.js");
 
 //connection settings
-const port = process.env.PORT || 3000;
+// it will check for a port in env variable...http.if no port is present then it takes 3000
+const port = 5000;
 
 //Create server
 const server = http.createServer(respond);
 
 //listen to client requests on the specific port, the port should be available
 server.listen(port, () => {
-    console.log(`listening on port: ${port}`);
+  console.log(`listening on port: ${port}`);
 });
